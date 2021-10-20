2eb3eea0a3f6f40d3192d7b9e2cc015403ab74e5
Max <bigin.maks@gmail.com>
2021-10-21 00:58:14 +0300

    edit router

--- src/manage.py
+++ src/manage.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative game_auth."""
 import os
 import sys
 
 
 def main():
-    """Run administrative game_auth."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
     try:
         from django.core.management import execute_from_command_line
