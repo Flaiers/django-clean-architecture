7fe550b61acdc732b08c2f11555a6bbcf45248a8
Max <bigin.maks@gmail.com>
2021-11-17 00:36:30 +0300

    fixes at container up

--- deployment/scripts/gunicorn.sh
+++ deployment/scripts/gunicorn.sh
@@ -2,6 +2,7 @@
 source env/bin/activate
 cd src/
 
+python manage.py collectstatic --noinput
 python manage.py migrate
 python build.py
 gunicorn --reload -b 0.0.0.0:${WEB_PORT} -c config/gunicorn.conf.py -k uvicorn.workers.UvicornWorker config.asgi:application
--- deployment/scripts/uvicorn.sh
+++ deployment/scripts/uvicorn.sh
@@ -2,6 +2,7 @@
 source env/bin/activate
 cd src/
 
+python manage.py collectstatic --noinput
 python manage.py migrate
 python build.py
 uvicorn --reload --host 0.0.0.0 --port ${WEB_PORT} config.asgi:application
